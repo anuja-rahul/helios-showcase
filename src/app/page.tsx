51c7778af94ef04de813ac58aafad868a23a7a30
Anuja Gunasinghe <a.r.gunasinghe@gmail.com>
2025-04-04 01:30:02 +0530

    Refactor layout: remove unused .main class, add About component, and update NavBar styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import About from "@/components/home/About";
 import Hero from "@/components/home/Hero";
 
 export default function Home() {
   return (
     <main className="relative main h-[200vh] bg-background text-white">
       <Hero />
+      <About />
     </main>
   );
 }
---
+++ src/components/home/About.tsx
@@ -0,0 +1,7 @@
+export default function NavBar() {
+  return (
+    <section className="relative h-dvh w-screen overflow-x-hidden bg-gray-600/10">
+      About
+    </section>
+  );
+}
