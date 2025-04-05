a5f4b3ba9092d31a5560cddc3d3b9b0ffa51214b
Anuja Gunasinghe <a.r.gunasinghe@gmail.com>
2025-04-05 17:19:48 +0530

    Refactor ColorVariantSection: enhance scrolling effects and update props for ColorScrollParent and ColorScrollSection components

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -1,7 +1,16 @@
+import ColorScrollParent, {
+  ColorScrollSection,
+} from "../ui/ColorVariantSection";
+
 export default function NavBar() {
   return (
-    <section className="relative h-dvh w-screen overflow-x-hidden bg-gray-600/10">
-      About
-    </section>
+    // <section className="relative h-dvh w-screen overflow-x-hidden bg-gray-600/10">
+      <ColorScrollParent className="h-[200vh] w-screen p-0 m-0 overflow-x-hidden">
+        <ColorScrollSection className="h-screen w-full">Section 1</ColorScrollSection>
+        <ColorScrollSection className="h-screen w-full" bgColor="#ff22ff" textColor="#0033ff">
+          Section 2
+        </ColorScrollSection>
+      </ColorScrollParent>
+    // </section>
   );
 }
--- src/components/ui/ColorVariantSection.tsx
+++ src/components/ui/ColorVariantSection.tsx
@@ -3,27 +3,83 @@
 import { cn } from "@/lib/util";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect } from "react";
+import { useSmoothScroller } from "./SmoothScrollContext";
 
 interface ColorVariantSectionProps {
-  children: React.ReactNode;
   bgColor?: string;
   textColor?: string;
+  children: React.ReactNode;
   className?: string;
 }
 
-export default function ColorVariantSection({
+export default function ColorScrollParent({
   children,
+  className,
+}: ColorVariantSectionProps) {
+  const lenis = useSmoothScroller();
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const sectionColor = document.querySelectorAll("[data-bg-color]");
+    sectionColor.forEach((section, i) => {
+      const prevBgColor =
+        i === 0 ? "" : (sectionColor[i - 1] as HTMLElement).dataset.bgColor;
+      const prevTextColor =
+        i === 0 ? "" : (sectionColor[i - 1] as HTMLElement).dataset.textColor;
+
+      ScrollTrigger.create({
+        trigger: section,
+        start: "top 50%",
+        onEnter: () => {
+          gsap.to(".scroller", {
+            backgroundColor: (section as HTMLElement).dataset.bgColor,
+            color: (section as HTMLElement).dataset.textColor,
+            overwrite: "auto",
+          });
+
+          gsap.to("[data-bg-color]", {
+            color: (section as HTMLElement).dataset.textColor,
+            backgroundColor: (section as HTMLElement).dataset.bgColor,
+            overwrite: "auto",
+          });
+        },
+        onLeaveBack: () => {
+          gsap.to(".scroller", {
+            backgroundColor: prevBgColor,
+            color: prevTextColor,
+            overwrite: "auto",
+          });
+
+          gsap.to("[data-bg-color]", {
+            color: prevTextColor,
+            backgroundColor: prevBgColor,
+            overwrite: "auto",
+          });
+        },
+      });
+    });
+
+    lenis?.on("scroll", ScrollTrigger.update);
+  }, [lenis]);
+
+  return <div className={cn(className)}>{children}</div>;
+}
+
+export const ColorScrollSection = ({
   bgColor = "#000000",
   textColor = "#ffffff",
   className,
-}: ColorVariantSectionProps) {
+  children,
+}: ColorVariantSectionProps) => {
   return (
     <section
-      bg-color={bgColor}
-      text-color={textColor}
-      className={cn("", className)}
+      data-bg-color={bgColor}
+      data-text-color={textColor}
+      className={cn("scroller", className)}
     >
       {children}
     </section>
   );
-}
+};
